97ae53554b34748addf3116b201fe068d6612c25
PPhatDev <leatsophat02@gmail.com>
2025-01-21 14:21:35 +0700

    🍪: Clean commnets

--- src/interfaces/cookie-options.ts
+++ src/interfaces/cookie-options.ts
@@ -1,7 +1,7 @@
 export interface CookieOptions {
-    expires?: Date | string | number; // Expiration date of the cookie
-    path?: string;                     // Path where the cookie is accessible
-    domain?: string;                   // Domain where the cookie is accessible
-    secure?: boolean;                  // Indicates if the cookie should only be transmitted over secure protocols
-    sameSite?: 'strict' | 'lax' | 'none'; // SameSite attribute for the cookie
+    expires?: Date | number;
+    path?: string;
+    domain?: string;
+    secure?: boolean;
+    sameSite?: 'Strict' | 'Lax' | 'None';
 }
